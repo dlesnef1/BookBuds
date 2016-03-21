7fea8e5b0f024fc1784680e1f9b854facd81f866
David <davidlesnefsky@gmail.com>
2016-03-21 09:46:49 -0400

    First commit

---
+++ src/main/java/com/ithaca/BookBudsApplication.java
@@ -0,0 +1,12 @@
+package com.ithaca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookBudsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookBudsApplication.class, args);
+	}
+}
