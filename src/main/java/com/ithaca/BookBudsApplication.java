5e84b3e57f011dd0b6e59e870c8a774344d8d141
David <davidlesnefsky@gmail.com>
2016-03-30 15:15:07 -0400

    Enabling cors

--- src/main/java/com/ithaca/BookBudsApplication.java
+++ src/main/java/com/ithaca/BookBudsApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class BookBudsApplication {
@@ -14,6 +17,16 @@ public class BookBudsApplication {
 		registrationBean.setFilter(new JwtFilter());
 		registrationBean.addUrlPatterns("/users/account");
 		return registrationBean;
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**");
+			}
+		};
 	}
 
 	public static void main(String[] args) {
