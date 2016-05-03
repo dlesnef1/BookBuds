629793d523adf9be5c8f7f03690883d69a56bd68
David <davidlesnefsky@gmail.com>
2016-05-03 14:19:15 -0400

    Cleaning up group controller

--- src/main/java/com/ithaca/group/GroupController.java
+++ src/main/java/com/ithaca/group/GroupController.java
@@ -1,6 +1,5 @@
 package com.ithaca.group;
 
-import com.ithaca.book.Book;
 import io.jsonwebtoken.Claims;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
