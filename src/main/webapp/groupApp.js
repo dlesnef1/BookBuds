8939e36bc163ddda153a23e335265cda0063aa30
NSBPittman <nicksbpittman@gmail.com>
2016-05-10 15:13:19 -0400

    and we're done

--- src/main/webapp/groupApp.js
+++ src/main/webapp/groupApp.js
@@ -5,7 +5,7 @@ var appModule = angular.module('myApp', []);
 
 appModule.controller('MainCtrl', ['mainService', '$scope', '$http',
     function (mainService, $scope, $http) {
-        $scope.greeting = 'Schlooby Dooby Doo';
+        $scope.greeting = 'Book Group';
         $scope.error = null;
         $scope.searched = false;
         $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
@@ -15,6 +15,8 @@ appModule.controller('MainCtrl', ['mainService', '$scope', '$http',
         $scope.token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJUb2J5IiwiaWQiOjF9.pRKQFSz0tEJvWeBO4ji85HWKHBnYEZ3GC7w4dxG2m54";
         $scope.groupCreated = null;
         $scope.posts = null;
+        $scope.isReply = false;
+        $scope.children = null;
 
 
 
@@ -44,16 +46,16 @@ appModule.controller('MainCtrl', ['mainService', '$scope', '$http',
 
                 return group;
             })
-        },
+        };
 
-        $scope.report = function(postId){
+        $scope.report = function(postId) {
             // var postId = 2;
-            mainService.report(postId).then(function(group){
+            mainService.report(postId).then(function (group) {
                 $scope.posts = group.posts;
 
                 return group;
             })
-        }
+        };
 
         $scope.reply = function () {
             mainService.reply($scope.groupId, $scope.text).then(function(response) {
@@ -64,8 +66,23 @@ appModule.controller('MainCtrl', ['mainService', '$scope', '$http',
 
         };
 
+        $scope.replyTo = function (postId, text) {
+            mainService.replyTo($scope.groupId, postId, text).then(function(response) {
+                $scope.posts = response.posts;
+                //$scope.children = response;
+                $scope.isReply = false;
+                return response;
+            });
 
+        };
 
+        $scope.setReply = function () {
+            $scope.isReply = true;
+        }
+
+        $scope.showReply = function () {
+            return $scope.isReply;
+        }
 
     }
 ]);
@@ -89,12 +106,24 @@ appModule.service('mainService', function ($http) {
 
             });
         },
+        replyTo: function (groupId, postId, text) {
+
+            var data = "text=" + text;
+            console.log(data);
+
+            return $http.post('http://localhost:8080/groups/' + groupId + "/" + postId, data).then(function (response) {
+                console.log(response.data);
+                return response.data;
+
+            });
+        },
         upvote: function (postId) {
             return $http.put('http://localhost:8080/groups/1/'+ postId +'/upvote'). then(function (response) {
                 console.log(response.data);
                 return response.data;
             });
         },
+
         report: function (postId) {
             return $http.put('http://localhost:8080/groups/1/'+ postId +'/report'). then(function (response) {
                 console.log(response.data);
